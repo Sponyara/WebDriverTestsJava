8b0440143543f675a78164bcb8271b87a9b18e53
Sponya <Sponyara@outlook.com>
2021-12-13 20:32:18 +0300

    Fix

--- src/test/java/WebDiverTest.java
+++ src/test/java/WebDiverTest.java
@@ -4,37 +4,36 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class WebDiverTest {
     @Test
     public void main() throws InterruptedException{
         System.setProperty("webdriver.gecko.driver","geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
         driver.get("https://nominex.io/ru");
-        Thread.sleep(2000);
         WebElement searchInput = driver.findElement(By.cssSelector(".space-x-16 > sl-button:nth-child(1)"));
         searchInput.click();
-        Thread.sleep(2000);
         searchInput = driver.findElement(By.cssSelector("sl-form.mt-20 > sl-input:nth-child(2)"));
         searchInput.click();
         searchInput.sendKeys("viteksilin@mail.ru");
-        Thread.sleep(1500);
         searchInput = driver.findElement(By.cssSelector("#current-password"));
         searchInput.click();
         searchInput.sendKeys("Qw321645");
-        Thread.sleep(1500);
         searchInput = driver.findElement(By.cssSelector("sl-button.w-full:nth-child(4)"));
         searchInput.click();
-        Thread.sleep(4000);
+        Thread.sleep(2000);
         searchInput = driver.findElement(By.cssSelector("ul.flex > li:nth-child(2)"));
         searchInput.click();
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#tab-5"));
+        searchInput = driver.findElement(By.cssSelector("div.overflow-hidden:nth-child(2) > div:nth-child(1) > sl-dropdown:nth-child(3) > sl-button:nth-child(1)"));
+        searchInput.click();
+        searchInput = driver.findElement(By.cssSelector("div.overflow-hidden:nth-child(2) > div:nth-child(1) > sl-dropdown:nth-child(3) > sl-menu:nth-child(2) > sl-menu-item:nth-child(1)"));
         searchInput.click();
         Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#tab-panel-5 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(9) > td:nth-child(2)"));
+        searchInput = driver.findElement(By.cssSelector("tr.ve-table-body-tr:nth-child(8) > td:nth-child(2)"));
         searchInput.click();
-        Thread.sleep(2000);
-        searchInput = driver.findElement(By.cssSelector("#tab-12"));
+        searchInput = driver.findElement(By.cssSelector("#tab-6"));
         searchInput.click();
         Thread.sleep(5000);
         driver.quit();
